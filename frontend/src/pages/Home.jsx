2e2c87d2e2040a185ead046f63db600dcfe8632b
Danish Shah <shanildanshah@gmail.com>
2026-02-04 12:00:00 +0530

    third push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner';
 import Movies from '../components/Movies';
+import Trailers from '../components/Trailers';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
     <Navbar/>
     <Banner/>
     <Movies/>
+    <Trailers/>
     </div>
   )
 }
